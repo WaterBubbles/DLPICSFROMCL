94256daec159c53e5eaccb7927bd8cd71365753a
-LinShu- <hagongcheng@gmail.com>
2017-04-13 16:57:11 -0700

    Update XSDDWM.py

--- XSDDWM.py
+++ XSDDWM.py
@@ -9,14 +9,14 @@ my_headers={'User-Agent' : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) Apple
     'Accept' : 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
     #'Accept-Encoding' : 'gzip',
     'Accept-Language' : 'zh-CN,zh;q=0.8,en;q=0.6,zh-TW;q=0.4'}
-qianzhui="htm_data/8/1704"
+qianzhui="htm_data/8/1702"
 for yeshu in range(1,7):
     address='http://www.t66y.com/thread0806.php?fid=8&search=&page='+str(yeshu)
     res=requests.get(address,headers=my_headers)
     soup = BeautifulSoup(res.text,'lxml')
     for res in soup.find_all('a'):
         res1=str(res.get('href'))
-        if(res1[0:15]=='htm_data/8/1702'):
+        if(res1[0:15]=='htm_data/8/1704'):
             res2=strinfo.sub('http://www.t66y.com/htm_data',res1)
             print(res2)
             time.sleep(random.uniform(1,3))
