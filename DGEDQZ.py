4374e72606f4093d90ac0d1b0f42fd4e4903437d
-LinShu- <hagongcheng@gmail.com>
2017-04-13 17:00:40 -0700

    Update DGEDQZ.py

--- DGEDQZ.py
+++ DGEDQZ.py
@@ -7,16 +7,16 @@ import re
 strinfo=re.compile('htm_data')
 my_headers={'User-Agent' : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36',
     'Accept' : 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-    'Accept-Encoding' : 'gzip',
+#    'Accept-Encoding' : 'gzip',
     'Accept-Language' : 'zh-CN,zh;q=0.8,en;q=0.6,zh-TW;q=0.4'}
-qianzhui="htm_data/16/1702"
+#qianzhui="htm_data/16/1702"
 for yeshu in range(4,7):
     address='http://www.t66y.com/thread0806.php?fid=16&search=&page='+str(yeshu)
     res=requests.get(address,headers=my_headers)
     soup = BeautifulSoup(res.text,'lxml')
     for res in soup.find_all('a'):
         res1=str(res.get('href'))
-        if(res1[0:16]=='htm_data/16/1702'):
+        if(res1[0:16]=='htm_data/16/1704'):
             res2=strinfo.sub('http://www.t66y.com/htm_data',res1)
             print(res2)
             time.sleep(random.uniform(1,3))
