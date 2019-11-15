3e5b4a81ff751f822fff00a239446d0356e932be
-LinShu- <hagongcheng@gmail.com>
2019-11-15 21:41:50 +0800

    Update DGEDQZ.py
    Nov.15, 2019, download pics from CL society.

--- DGEDQZ.py
+++ DGEDQZ.py
@@ -1,3 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
 import requests
 import time
 import random
@@ -10,20 +17,22 @@ my_headers={'User-Agent' : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) Apple
 #    'Accept-Encoding' : 'gzip',
     'Accept-Language' : 'zh-CN,zh;q=0.8,en;q=0.6,zh-TW;q=0.4'}
 #qianzhui="htm_data/16/1702"
-for yeshu in range(4,7):
+for yeshu in range(1,2):
     address='http://www.t66y.com/thread0806.php?fid=16&search=&page='+str(yeshu)
     res=requests.get(address,headers=my_headers)
     soup = BeautifulSoup(res.text,'lxml')
+
     for res in soup.find_all('a'):
         res1=str(res.get('href'))
-        if(res1[0:16]=='htm_data/16/1705'):
+        if(res1[0:16]=='htm_data/1911/16'):
             res2=strinfo.sub('http://www.t66y.com/htm_data',res1)
             print(res2)
             time.sleep(random.uniform(1,3))
             res_sub=requests.get(res2,headers=my_headers)
             soup_sub = BeautifulSoup(res_sub.text,'lxml')
             for res_sub in soup_sub.find_all('input'):
-                ress=str(res_sub.get('src'))
+                ress=str(res_sub.get('data-src'))
+                print(ress)
                 if(len(ress)>10):
                     fname=ress.split('/')[-1]
                     try:
